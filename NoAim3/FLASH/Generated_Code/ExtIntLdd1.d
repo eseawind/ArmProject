f7160915c60dadf24312b5699b80936307d05e0d
BH1TAN <yxtccty@163.com>
2015-05-29 11:42:25 +0800

    Revert "Revert "20150529第一次上传""
    This reverts commit ad78f6aa8fd253bdef5169fc948690980a1015d1.

---
+++ NoAim3/FLASH/Generated_Code/ExtIntLdd1.d
@@ -0,0 +1,61 @@
+Generated_Code/ExtIntLdd1.o: ../Generated_Code/ExtIntLdd1.c \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ ../Generated_Code/Encoder1.h ../Generated_Code/PE_Types.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ ../Generated_Code/PE_Error.h ../Generated_Code/PE_Const.h \
+ ../Generated_Code/IO_Map.h ../Generated_Code/ExtIntLdd1.h \
+ ../Generated_Code/Cpu.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/GPIO_PDD.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h \
+ E:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PORT_PDD.h
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+../Generated_Code/Encoder1.h:
+
+../Generated_Code/PE_Types.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/IO_Map.h:
+
+../Generated_Code/ExtIntLdd1.h:
+
+../Generated_Code/Cpu.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/GPIO_PDD.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h:
+
+E:/Freescale/CW\ MCU\ v10.5/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PORT_PDD.h:
---
+++ NoAim3/FLASH/make4188-1.bat
@@ -0,0 +1,2 @@
+@echo off
+echo 'Building target: NoAim3.elf'
